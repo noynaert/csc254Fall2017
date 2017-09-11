1f9aeef1036297762e0d61e8c216450ef0e0c469
J. Evan Noynaert <noynaert@missouriwestern.edu>
2017-09-11 18:19:27 -0500

    Printing a table

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	     double number;
-	     double result;
-
-	     number = 7.2;
-	     result = calculateSquare(number);
-
-         printValue(number, result);
-         printValue(16,calculateSquare(Math.pow(4,1+1)));
-
-        System.out.printf("Maybe the value of %f to the 6th power is %f\n", 2.0, calculateSixth(2) );
-
-        System.out.println("The log2 of 15 is " + log2(15));
-
+        int[] ranges = {26, 52, 62, 67, 72};
+        printTable(5, ranges);
     }
 
-
-    public static double log2(double number){
-        double result;
-        result = Math.log(number) / Math.log(2);
-        return result;
-    }
-
-    public static double calculateSquare(double x){
-        double result;                        //scope
-        result = x * x;
-        return result;  // as calculateSquare
-    }
-    public static double calculateCube(double number){
-        return number * number * number;
-    }
-    public static void printValue(double original, double afterCalculation){
-        System.out.printf("The square of %f is %f\n", original, afterCalculation);
+    public static void printTable(int rows, int[] columnValues){
+        printHeading(columnValues);
+        for(int i = 1;i<=rows;i++){
+            System.out.printf("L=%2d", i);
+            for(int j=0;j<columnValues.length;j++){
+                System.out.printf("%9.1f",(double)Math.log(columnValues[j]+i));
+            }
+            System.out.println();
+        }
 
     }
-    public static double calculateSixth(double x){
-        double result = calculateSquare(calculateCube(x));
-        return result;
+    public static void printHeading(int[] columnValues){
+        System.out.print("++++");
+        for(int i=0;i<columnValues.length;i++){
+            System.out.printf(".....R=%2d", columnValues[i]);
+        }
+        System.out.println();
     }
 
 }
