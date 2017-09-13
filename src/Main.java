4fd5712ba2dc22d23c8f8e7b536223176394b390
J. Evan Noynaert <noynaert@missouriwestern.edu>
2017-09-13 17:20:19 -0500

    Basic reading from the command line.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] ranges = {26, 52, 62, 67, 72};
-        printTable(5, ranges);
-    }
 
-    public static void printTable(int rows, int[] columnValues){
-        printHeading(columnValues);
-        for(int i = 1;i<=rows;i++){
-            System.out.printf("L=%2d", i);
-            for(int j=0;j<columnValues.length;j++){
-                System.out.printf("%9.1f",(double)Math.log(columnValues[j]+i));
-            }
-            System.out.println();
+        int number;
+        String word;
+        String[] words;
+
+        System.out.printf("There are %d words on the command line.\n", args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
         }
     }
-    public static void printHeading(int[] columnValues){
-        System.out.print("++++");
-        for(int i=0;i<columnValues.length;i++){
-            System.out.printf(".....R=%2d", columnValues[i]);
-        }
-        System.out.println();
-    }
-
 }
