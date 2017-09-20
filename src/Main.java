d28a1b294c48519b434b23cd8156753228177d52
J. Evan Noynaert <noynaert@missouriwestern.edu>
2017-09-20 18:21:45 -0500

    messing with indexOf()

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
 public class Main {
 
     public static void main(String[] args) {
-	  String[] words = {"Dog", "Cat", "Fish", "Bird", "Cat"};
-	  boolean found = contains("dog",words, words.length);
-	  System.out.println(found);
+
+        String s = "abcDEF123z?";
+
+//        for(int i = 0;  i<s.length(); i++){
+//            System.out.printf("The character at %d is %c\n", i, s.charAt(i));
+//        }
+//        for(char ch = 'a'; ch < 'z'; ch++){
+//            System.out.printf(" %c %d\n", ch-32, (int)ch-32);
+//        }
+//
+//        char ch = ' ';
+//        while(ch < '~'){
+//            System.out.printf("%c %d\n", ch, s.indexOf(ch));
+//            ch++;
+//        }
+         System.out.println( contains(s, 'a', 'b'));
+
     }
 
-    private static boolean contains(String target, String[] list, int n) {
-         boolean found = false;
+    public static boolean contains(String s, char start, char end){
+        boolean found = false;
 
-         int i = 0;
-         while(!found && i<n) {
-             found = (  target.equalsIgnoreCase(list[i])  );
-             //found = target.toLowerCase().equals(list[i].toLowerCase());
-             i++;
-         }
-         return found;
+        char ch = start;
+        while(!found && ch <= end){
+            found = (s.indexOf(ch) > -1);
+            ch++;
+        }
 
+
+        return found;
     }
-
-
 }
